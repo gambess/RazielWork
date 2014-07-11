6665898a7cad2fde6b41270a7d037d13686a8e8d
Raziel Valle <raziel.valle@fractaliasoftware.com>
2014-07-11 14:59:08 +0200

    Raziel Valle Miranda; Primeros cambios para la version 1.4

---
+++ src/Pi2/Fractalia/SmsBundle/Form/NombretsolType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Pi2\Fractalia\SmsBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class NombretsolType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nombre')
+            ->add('fechaModificacion')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Pi2\Fractalia\SmsBundle\Entity\Nombretsol'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'pi2_fractalia_smsbundle_nombretsol';
+    }
+}
